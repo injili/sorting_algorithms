03cad48cea071d2a8436abf9bf4a95302188cad7
injili <nyarekigospel@gmail.com>
2023-06-23 00:43:42 -0700

    bubble sort

---
+++ 0-bubble_sort.c
@@ -0,0 +1,14 @@
+#include "sort.h"
+/**
+ * bubble_sort - a function that sort an array of
+ * integers in ascending order using the Bubble sort algorithm
+ *
+ * @array: array passed to the function
+ * @size: size of the array
+ *
+ * Return: no return value
+ */
+
+void bubble_sort(int *array, size_t size)
+{
+}
