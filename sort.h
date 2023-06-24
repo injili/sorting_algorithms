e478dc6519f1c3c46483804460a43b2ad945170d
abraham-ship <bettabraham11@gmail.com>
2023-06-24 01:29:26 -0700

    fix header file

--- sort.h
+++ sort.h
@@ -1,6 +1,8 @@
 #ifndef SORT_H
 #define SORT_H
 
+#include <stddef.h>
+
 /**
  * struct listint_s - Doubly linked list node
  *
@@ -10,13 +12,13 @@
  */
 typedef struct listint_s
 {
-    const int n;
-    struct listint_s *prev;
-    struct listint_s *next;
+	const int n;
+	struct listint_s *prev;
+	struct listint_s *next;
 } listint_t;
 
 void bubble_sort(int *array, size_t size);
-void print_array(const int*array, size_t size);
+void print_array(const int *array, size_t size);
 void print_list(const listint_t *list);
 void insertion_sort_list(listint_t **list);
 void selection_sort(int *array, size_t size);
