76ef7afff6487e306d25764ec40dc581ac50a4ee
abraham-ship <bettabraham11@gmail.com>
2023-06-24 03:00:26 -0700

    fix swapping in algorithm

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -24,19 +24,22 @@ void swap(int *a, int *b)
 int partition(int *array, int low, int high, size_t size)
 {
 	int pivot = array[high];
-	int i = low - 1;
+	int i = low;
 	int j;
 
-	for (j = low; j <= high - 1; j++)
+	for (j = low; j < high; j++)
 	{
 		if (array[j] < pivot)
 		{
-			i++;
 			swap(&array[i], &array[j]);
+			if (array[i] != array[j])
+				print_array(array, size);
+			i++;
 		}
 	}
-	swap(&array[i + 1], &array[high]);
-	print_array(array, size);
+	swap(&array[i], &array[high]);
+	if (array[i] != array[high])
+		print_array(array, size);
 
 	return (i);
 }
