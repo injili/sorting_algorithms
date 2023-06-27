c2dba68c3bcebec251ac7c82f2c6c7d55d32cba9
abraham-ship <bettabraham11@gmail.com>
2023-06-27 05:34:22 -0700

    insertion list

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -35,7 +35,7 @@ void insertion_sort_list(listint_t **list)
 {
 	listint_t *current;
 
-	if (*list == NULL || (*list)->next == NULL)
+	if (list == NULL || *list == NULL || (*list)->next == NULL)
 		return;
 
 	current = (*list)->next;
